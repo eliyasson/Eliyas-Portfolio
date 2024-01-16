33d9e1e0f72e33b750be98d7cd61ee096df9be47
eliyas <eliyasson11@gmail.com>
2024-01-16 14:21:04 +0200

    add some css

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
 const name = "Eliyas";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
