7f5ded68021d98bd65f0b355584d5152b39fcd54
eliyas <eliyasson11@gmail.com>
2024-01-21 23:53:22 +0200

    added header details

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ function About() {
         <h2 class="section-title" id="about-title">About Me</h2>
         <div class="con">
             <div class="text-container">
-                <h3>I'm a lead <span>Web Developer</span> based in <span>Helsinki</span> willing to be a Full stack Developer in future. I create stunnig, eye-catching and user-friendly websites. I am highly experienced in web design and development.</h3>
+                <h3>I'm a <span>front-end developer</span> based in <span>Helsinki</span> willing to be a Full stack Developer in future. I am currently a student at Stadin AO, pursuing studies Ohjelmistokehittäjä. Simultaneously, I am a self-taught front-end developer through Udemy, aiming to enhance my skills for future endeavors in front-end development</h3>
             </div>
         </div>
     </div>
