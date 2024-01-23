e29d6809e7a93ad3e3a444f499b1275695effb8a
eliyas <eliyasson11@gmail.com>
2024-01-23 21:26:14 +0200

    update

--- src/Footer.js
+++ src/Footer.js
@@ -9,11 +9,11 @@ function Footer() {
         <div className="follow group">
           <h3>Follow</h3>
           <ul>
-          <a href="https://www.facebook.com/jesus.islord99/" id="fb" target="_blank"><i className="fab fa-facebook-f"></i></a>
-          <a href='https://www.instagram.com/eliiyas_t/?hl=en'id="insta" target="_blank"><i class="fab fa-instagram"></i></a>
-          <a href="https://www.linkedin.com/in/eliyas-kassaye/" id="linkedin" target="_blank"><i class="fab fa-linkedin"></i></a>
-          <a href="https://api.whatsapp.com/send?phone=+358405390711" id="whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
-          <a href="https://github.com/eliyasson" id="git" target="_blank"><i class="fab fa-github"></i></a>
+          <a href="https://www.facebook.com/jesus.islord99/" id="fb" target="_blank" rel="noreferrer"><i className="fab fa-facebook-f"></i></a>
+          <a href='https://www.instagram.com/eliiyas_t/?hl=en'id="insta" target="_blank" rel="noreferrer"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.linkedin.com/in/eliyas-kassaye/" id="linkedin" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
+          <a href="https://api.whatsapp.com/send?phone=+358405390711" id="whatsapp" target="_blank" rel="noreferrer"><i class="fab fa-whatsapp"></i></a>
+          <a href="https://github.com/eliyasson" id="git" target="_blank" rel="noreferrer"><i class="fab fa-github" ></i></a>
           </ul>
         </div>
       </div>
